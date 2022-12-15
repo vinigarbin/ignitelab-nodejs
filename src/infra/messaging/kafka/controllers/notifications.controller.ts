c39111c0f2b8fc6bec19ab2ddce066ff34d79629
Vinicius Garbin <vinigarbin@gmail.com>
2022-12-15 20:55:55 -0300

    Implements kafka

---
+++ src/infra/messaging/kafka/controllers/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { SendNotification } from '@application/use-cases/send-notification';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+interface SendNotificationPayload {
+  content: string;
+  category: string;
+  recipientId: string;
+}
+
+@Controller()
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @EventPattern('notifications.send-notification')
+  async handleSendNotification(@Payload() content: SendNotificationPayload) {
+    console.log(content);
+    await this.sendNotification.execute(content);
+  }
+}
